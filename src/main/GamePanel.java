10d16706169011872463a826c6c3a2a7c3dda229
Noxx <Noxx@DESKTOP-DDU20H4.fritz.box>
2023-05-26 20:26:46 +0200

    Added FPS Timer in Console

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,9 +18,9 @@ public class GamePanel extends JPanel implements Runnable {
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol; // 768 pixel
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixel
-	
+
 	// FPS
-	
+
 	int FPS = 60;
 
 	KeyHandler keyH = new KeyHandler();
@@ -47,41 +47,43 @@ public class GamePanel extends JPanel implements Runnable {
 
 	@Override
 	public void run() {
-		
-		double drawInterval = 1000000000/FPS;
+
+		double drawInterval = 1000000000 / FPS;
 		double delta = 0;
 		long lastTime = System.nanoTime();
+		long timer = 0;
+		int drawCount = 0;
 		long currentTime;
-		
+
 		while (gameThread != null) {
-			
+
 			currentTime = System.nanoTime();
 			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
 			lastTime = currentTime;
-			
-			if (delta>=1) {
+
+			if (delta >= 1) {
 				update();
 				repaint();
 				delta--;
+				drawCount++;
 			}
-			
-			
-			
-			
+			if (timer >= 1000000000) {
+				System.out.println("FPS:" + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
 		}
 	}
 
 	public void update() {
 		if (keyH.upPressed == true) {
 			playerY -= playerSpeed;
-		} 
-		else if (keyH.downPressed == true) {
+		} else if (keyH.downPressed == true) {
 			playerY += playerSpeed;
-		} 
-		else if (keyH.leftPressed == true) {
+		} else if (keyH.leftPressed == true) {
 			playerX -= playerSpeed;
-		} 
-		else if (keyH.rightPressed == true) {
+		} else if (keyH.rightPressed == true) {
 			playerX += playerSpeed;
 		}
 
@@ -94,7 +96,7 @@ public class GamePanel extends JPanel implements Runnable {
 		Graphics2D g2 = (Graphics2D) g;
 
 		g2.setColor(Color.white);
-		
+
 		g2.fillRect(playerX, playerY, tileSize, tileSize);
 
 		g2.dispose();

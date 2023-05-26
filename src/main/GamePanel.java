7fab48f35abba09a291357d35b0a9a0bc0d6a2b0
Noxx <Noxx@DESKTOP-DDU20H4.fritz.box>
2023-05-26 20:12:33 +0200

    Added KeyHandler

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -19,13 +19,21 @@ public class GamePanel extends JPanel implements Runnable {
 	final int screenWidth = tileSize * maxScreenCol; // 768 pixel
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixel
 
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+
+	// Set player's default Position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 
 	public GamePanel() {
 
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 
 	public void startGameThread() {
@@ -42,7 +50,19 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 
 	public void update() {
-		
+		if (keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		} 
+		else if (keyH.downPressed == true) {
+			playerY += playerSpeed;
+		} 
+		else if (keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		} 
+		else if (keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
+
 	}
 
 	public void paintComponent(Graphics g) {
@@ -52,8 +72,9 @@ public class GamePanel extends JPanel implements Runnable {
 		Graphics2D g2 = (Graphics2D) g;
 
 		g2.setColor(Color.white);
-		g2.fillRect(100, 100, tileSize, tileSize);
 		
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
+
 		g2.dispose();
 	}
 }

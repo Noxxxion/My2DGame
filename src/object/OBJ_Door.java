e416551d184875e2023140aaccf11b851eed2b3e
Rubruck Benjamin <Rubruck Benjamin@192.168.151.114>
2023-06-05 15:47:42 +0200

    Added collision

--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class OBJ_Door extends SuperObject{
+public class OBJ_Door extends SuperObject {
 	public OBJ_Door() {
 		name = "Door";
 		try {
 			image = ImageIO.read(getClass().getResourceAsStream("/objects/door.png"));
-			
+
 		} catch (IOException e) {
-			e.printStackTrace();		}
+			e.printStackTrace();
+		}
+		collision = true;
 	}
 }
